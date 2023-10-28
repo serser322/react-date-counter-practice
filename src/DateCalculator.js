c4ac31e53062752f2e15a004ed47a310747969b1
Wei Chiao Chen <www322.joe@gmail.com>
2023-10-28 23:59:14 +0800

    feat:Add DateMessage

--- src/DateCalculator.js
+++ src/DateCalculator.js
@@ -1,38 +1,40 @@
-import { useState } from 'react'
-import './DateCalculator.css'
+import { useState } from "react";
+import "./DateCalculator.css";
+import DateMessage from "./DateMessage";
 
 export default function DateCalculator() {
-  const [step, setStep] = useState(0)
-  const [count, setCount] = useState(0)
+  const [step, setStep] = useState(0);
+  const [count, setCount] = useState(0);
 
   const minusStep = () => {
-    setStep (currentValue => currentValue -1)
-  }
+    setStep((currentValue) => currentValue - 1);
+  };
   const addStep = () => {
-    setStep((currentValue) => currentValue + 1 )
-  }
+    setStep((currentValue) => currentValue + 1);
+  };
 
   const minusCount = () => {
-    setCount( currentValue => currentValue - step)
-  }
+    setCount((currentValue) => currentValue - step);
+  };
 
   const addCount = () => {
-    setCount( currentValue => currentValue + step)
-  }
+    setCount((currentValue) => currentValue + step);
+  };
 
   return (
-    <div className='container'>
-      <div className='step_counter'>
+    <div className="container">
+      <div className="step_counter">
         <button onClick={minusStep}>-</button>
         <div>Step:{step}</div>
         <button onClick={addStep}>+</button>
       </div>
-      <div className='count_counter'>
+      <div className="count_counter">
         <button onClick={minusCount}>-</button>
         <div>Count:{count}</div>
         <button onClick={addCount}>+</button>
       </div>
       <div>Current Value : {count} </div>
+      <DateMessage dateCount={count} />
     </div>
-  )
+  );
 }
---
+++ src/DateMessage.js
@@ -0,0 +1,3 @@
+export default function DateMessage() {
+  return <div></div>;
+}
